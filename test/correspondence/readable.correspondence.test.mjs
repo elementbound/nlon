8e57438890a3fc591d7cb9f6b7c4137a6f5f0a63
Tamás Gálffy <ezittgtx@gmail.com>
2023-02-10 14:47:23 +0100

    Feat/consolidate correspondence (#9)
    feat: Consolidate Correspondence APIs

---
+++ test/correspondence/readable.correspondence.test.mjs
@@ -0,0 +1,347 @@
+import { describe, it, beforeEach, mock } from 'node:test'
+import assert from 'node:assert'
+import { Message, MessageError, MessageHeader, MessageTypes } from '../../lib/protocol.mjs'
+import { ReadableCorrespondence } from '../../lib/correspondence/readable.correspondence.mjs'
+import { CorrespondenceError } from '../../lib/error.mjs'
+import { objectify } from '../utils.mjs'
+
+describe('ReadableCorrespondence', () => {
+  /** @type {ReadableCorrespondence} */
+  let correspondence
+
+  /** @type {MessageHeader} */
+  let header
+
+  beforeEach(() => {
+    header = new MessageHeader()
+    correspondence = new ReadableCorrespondence({
+      header
+    })
+  })
+
+  // {{{ Events
+  it('should emit data', () => {
+    // Given
+    const handler = mock.fn()
+    correspondence.on('data', handler)
+
+    const expected = 'test data'
+
+    // When
+    correspondence.handle(new Message({
+      header: new MessageHeader({
+        subject: 'test'
+      }),
+
+      type: MessageTypes.Data,
+
+      body: expected
+    }))
+
+    // Then
+    const calls = handler.mock.calls
+    assert.equal(calls?.length, 1, 'No event was emitted!')
+    assert.deepStrictEqual(calls[0].arguments, [expected, false])
+  })
+
+  it('should emit finish', () => {
+    // Given
+    const handler = mock.fn()
+    correspondence.on('finish', handler)
+
+    // When
+    correspondence.handle(new Message({
+      header: new MessageHeader({
+        subject: 'test'
+      }),
+
+      type: MessageTypes.Finish
+    }))
+
+    // Then
+    assert.equal(handler.mock.callCount(), 1, 'No event was emitted!')
+  })
+
+  it('should emit data and finish', () => {
+    // Given
+    const dataHandler = mock.fn()
+    const finishHandler = mock.fn()
+
+    correspondence.on('data', dataHandler)
+    correspondence.on('finish', finishHandler)
+
+    const expected = 'test data'
+
+    // When
+    correspondence.handle(new Message({
+      header: new MessageHeader({
+        subject: 'test'
+      }),
+
+      type: MessageTypes.Finish,
+
+      body: expected
+    }))
+
+    // Then
+    assert.equal(dataHandler.mock.callCount(), 1, 'No data event was emitted!')
+    assert.deepStrictEqual(
+      dataHandler.mock.calls[0]?.arguments, [expected, true]
+    )
+
+    assert.equal(finishHandler.mock.callCount(), 1, 'No finish event emitted!')
+  })
+
+  it('should emit error', () => {
+    // Given
+    const handler = mock.fn()
+    correspondence.on('error', handler)
+
+    const error = new MessageError({ type: 'TestError', message: 'test' })
+    const expected = new CorrespondenceError(error)
+
+    // When
+    correspondence.handle(new Message({
+      header: new MessageHeader({
+        subject: 'test'
+      }),
+
+      type: MessageTypes.Error,
+      error
+    }))
+
+    // Then
+    assert.equal(handler.mock.callCount(), 1, 'No error event was emitted!')
+    assert.deepStrictEqual(handler.mock.calls[0].arguments, [expected])
+  })
+
+  /// }}}
+
+  // {{{ next
+  it('should return chunk on next', async () => {
+    // Given
+    const expected = 'chunk'
+    const message = new Message({
+      header,
+      type: MessageTypes.Data,
+      body: expected
+    })
+
+    // When
+    const actual = correspondence.next()
+    correspondence.handle(message)
+
+    // Then
+    assert.deepEqual(objectify(await actual), objectify(expected))
+  })
+
+  it('should return symbol on next', async () => {
+    // Given
+    const expected = ReadableCorrespondence.End
+    const message = new Message({
+      header,
+      type: MessageTypes.Finish
+    })
+
+    // When
+    const actual = correspondence.next()
+    correspondence.handle(message)
+
+    // Then
+    assert.equal(await actual, expected)
+  })
+
+  it('should call handlers on next', async () => {
+    // Given
+    const data = 'test'
+    const message = new Message({
+      header,
+      type: MessageTypes.Data,
+      body: data
+    })
+
+    const handlers = [
+      mock.fn(),
+      mock.fn(),
+      mock.fn()
+    ]
+
+    const expectedArguments = [data, header, {}]
+
+    // When
+    const promise = correspondence.next(...handlers)
+    correspondence.handle(message)
+    await promise
+
+    // Then
+    handlers.forEach(handler => {
+      assert.equal(handler.mock.callCount(), 1)
+      assert.deepStrictEqual(handler.mock.calls[0].arguments, expectedArguments)
+    })
+  })
+
+  it('should throw on next if handler throws', async () => {
+    // Given
+    const message = new Message({
+      header,
+      type: MessageTypes.Data,
+      body: 'chunk'
+    })
+
+    const handler = () => { throw new Error('test') }
+
+    // When
+    const promise = correspondence.next(handler)
+    correspondence.handle(message)
+
+    // Then
+    await assert.rejects(promise)
+  })
+
+  it('should throw on next if error', async () => {
+    // Given
+    const message = new Message({
+      header,
+      type: MessageTypes.Error,
+      error: new MessageError({ type: 'test', message: 'test' })
+    })
+
+    // When
+    const promise = correspondence.next()
+    correspondence.handle(message)
+
+    // Then
+    await assert.rejects(promise)
+  })
+
+  it('should throw on next if unreadable', async () => {
+    // When
+    correspondence.handle(new Message({ header, type: MessageTypes.Finish }))
+
+    // Then
+    assert(!correspondence.readable, 'Correspondence still readable!')
+    await assert.rejects(correspondence.next())
+  })
+  // }}}
+
+  // {{{all
+  it('should return chunks on all', async () => {
+    // Given
+    const chunks = ['hello', 'world']
+    const messages = chunks.map(chunk => new Message({
+      header,
+      type: MessageTypes.Data,
+      body: chunk
+    }))
+    const finishMessage = new Message({ header, type: MessageTypes.Finish })
+
+    // When
+    const generator = correspondence.all()
+
+    // Then
+    for (const message of messages) {
+      const promise = generator.next()
+      correspondence.handle(message)
+      const actual = await promise
+      const expected = { done: false, value: message.body }
+      assert.deepStrictEqual(actual, expected)
+    }
+
+    const promise = generator.next()
+    correspondence.handle(finishMessage)
+    const actual = await promise
+    const expected = { done: true, value: undefined }
+    assert.deepStrictEqual(actual, expected)
+  })
+
+  it('should call handlers on all', async () => {
+    // Given
+    const message = new Message({
+      header,
+      type: MessageTypes.Finish,
+      body: 'test'
+    })
+
+    const handlers = [
+      mock.fn(),
+      mock.fn()
+    ]
+
+    const expectedArguments = ['test', header, {}]
+
+    // When
+    const generator = correspondence.all(...handlers)
+    const promise = generator.next()
+    correspondence.handle(message)
+    await promise
+
+    // Then
+    handlers.forEach(handler => {
+      assert.equal(handler.mock.callCount(), 1, 'Handler was not called!')
+      assert.deepStrictEqual(handler.mock.calls[0].arguments, expectedArguments)
+    })
+  })
+
+  it('should throw on all if handler throws', async () => {
+    // Given
+    const message = new Message({
+      header,
+      type: MessageTypes.Finish,
+      body: 'test'
+    })
+
+    const handler = () => { throw new Error('test') }
+
+    // When
+    const generator = correspondence.all(handler)
+    const promise = generator.next()
+    correspondence.handle(message)
+
+    // Then
+    await assert.rejects(promise)
+  })
+
+  it('should throw on all if error', async () => {
+    // Given
+    const message = new Message({
+      header,
+      type: MessageTypes.Error,
+      error: new MessageError({ type: 'test', message: 'test' })
+    })
+
+    // When
+    const generator = correspondence.all()
+    const promise = generator.next()
+    correspondence.handle(message)
+
+    // Then
+    assert.rejects(promise)
+  })
+
+  it('should throw on all if unreadable', async () => {
+    // Given
+    correspondence.handle(new Message({ header, type: MessageTypes.Finish }))
+
+    // When
+    const generator = correspondence.all()
+    const promise = generator.next()
+
+    // Then
+    assert.rejects(promise)
+  })
+  // }}}
+
+  // {{{ wrap
+  it('should finish when wrap proxy is finished', () => {
+    // Given
+    const readable = ReadableCorrespondence.wrap(correspondence)
+
+    // When
+    readable.handle(new MessageHeader({ header, type: MessageTypes.Finish }))
+
+    // Then
+    assert(!readable.readable, 'Proxy still readable!')
+    assert(!correspondence.readable, 'Correspondence still readable!')
+  })
+  // }}}
+})
--- test/server.test.mjs
+++ test/server.test.mjs
@@ -1,10 +1,10 @@
 import { describe, it, beforeEach, mock } from 'node:test'
 import assert from 'node:assert'
-import { Server } from '../../lib/server/server.mjs'
-import { InspectableStream } from '../inspectable.stream.mjs'
-import { Message, MessageError, MessageHeader, MessageTypes } from '../../lib/protocol.mjs'
-import { StreamingError, InvalidMessageError } from '../../lib/error.mjs'
-import { objectify, send, sleep } from '../utils.mjs'
+import { Server } from '../lib/server.mjs'
+import { InspectableStream } from './inspectable.stream.mjs'
+import { Message, MessageError, MessageHeader, MessageTypes } from '../lib/protocol.mjs'
+import { StreamingError, InvalidMessageError } from '../lib/error.mjs'
+import { objectify, send, sleep } from './utils.mjs'
 
 describe('Server', () => {
   /** @type {Server} */
@@ -20,15 +20,11 @@ describe('Server', () => {
     server.connect(stream)
   })
 
-  it('should call all handlers', async () => {
+  // {{{ Correspondence handler
+  it('should call handler', async () => {
     // Given
-    const handlers = [
-      mock.fn(),
-      mock.fn(),
-      mock.fn((_request, response, _context) => response.finish())
-    ]
-
-    server.handle('test', ...handlers)
+    const handler = mock.fn()
+    server.handle('test', handler)
 
     // When
     await send(stream, new Message({
@@ -38,130 +34,15 @@ describe('Server', () => {
     }))
 
     // Then
-    handlers.every(handler =>
-      assert.equal(handler.mock.callCount(), 1, 'Handler was not called!')
-    )
-  })
-
-  it('should await async handlers', async () => {
-    // Given
-    const delay = 50
-    const correspondenceId = 'test-id'
-    const subject = 'test-ASnc'
-
-    const asyncHandler = async (_request, response) => {
-      await sleep(delay)
-      response.write('Hello')
-    }
-
-    const syncHandler = (_request, response) => {
-      response.finish('world!')
-    }
-
-    const expected = [
-      new Message({
-        header: {
-          subject,
-          correspondenceId
-        },
-
-        type: MessageTypes.Data,
-        body: 'Hello'
-      }),
-      new Message({
-        header: {
-          subject,
-          correspondenceId
-        },
-
-        type: MessageTypes.Finish,
-        body: 'world!'
-      })
-    ]
-
-    server.handle(subject, asyncHandler, syncHandler)
-
-    // When
-    send(stream, new Message({
-      header: new MessageHeader({
-        subject,
-        correspondenceId
-      })
-    }))
-    await sleep(delay + 10)
-
-    // Then
-    const actual = stream.multiJSON()
-
-    assert.deepStrictEqual(
-      actual.map(objectify),
-      expected.map(objectify),
-      'Responses don\'t match expected order or content!')
-  })
-
-  it('should stop handlers after response is finished', async () => {
-    // Given
-    const handlers = [
-      mock.fn(),
-      mock.fn((_request, response, _context) => response.finish()),
-      mock.fn()
-    ]
-
-    server.handle('test-finish', ...handlers)
-
-    // When
-    await send(stream, new Message({
-      header: new MessageHeader({
-        subject: 'test-finish'
-      })
-    }))
-
-    // Then
-    assert.equal(handlers[0].mock.callCount(), 1,
-      'First handler was not called!')
-    assert.equal(handlers[1].mock.callCount(), 1,
-      'Second handler was not called!')
-    assert.equal(handlers[2].mock.callCount(), 0,
-      'Third handler was called!')
-  })
-
-  it('should stop handlers after response is errored', async () => {
-    // Given
-    const handlers = [
-      mock.fn(),
-      mock.fn((_request, response, _context) =>
-        response.error(new MessageError({ type: 'test', message: 'test' }))),
-      mock.fn()
-    ]
-
-    server.handle('test-error', ...handlers)
-
-    // When
-    await send(stream, new Message({
-      header: new MessageHeader({
-        subject: 'test-error'
-      })
-    }))
-
-    // Then
-    assert.equal(handlers[0].mock.callCount(), 1,
-      'First handler was not called!')
-    assert.equal(handlers[1].mock.callCount(), 1,
-      'Second handler was not called!')
-    assert.equal(handlers[2].mock.callCount(), 0,
-      'Third handler was called!')
+    assert.equal(handler.mock.callCount(), 1, 'Handler was not called!')
   })
 
   it('should emit error on unfinished response', async () => {
-    // Given
-    const handlers = [
-      mock.fn(),
-      mock.fn()
-    ]
-
+    // Gven
+    const handler = mock.fn()
     const errorListener = mock.fn()
 
-    server.handle('test-unfinished', ...handlers)
+    server.handle('test-unfinished', handler)
     server.on('error', errorListener)
 
     // When
@@ -175,14 +56,10 @@ describe('Server', () => {
     assert(errorListener.mock.callCount(), 'No error was emitted!')
   })
 
-  it('should call default handlers', async () => {
+  it('should call default handler', async () => {
     // Given
-    const defaultHandlers = [
-      mock.fn(),
-      mock.fn((_request, response, _context) => response.finish())
-    ]
-
-    server.defaultHandlers(...defaultHandlers)
+    const defaultHandler = mock.fn()
+    server.defaultHandler(defaultHandler)
 
     // When
     await send(stream, new Message({
@@ -192,12 +69,13 @@ describe('Server', () => {
     }))
 
     // Then
-    defaultHandlers.forEach(handler =>
-      assert.equal(handler.mock.callCount(), 1,
-        'Default handler was not called!'
-      ))
+    assert.equal(defaultHandler.mock.callCount(), 1,
+      'Default handler was not called!'
+    )
   })
+  // }}}
 
+  // {{{ Exception handlers
   it('should call exception handlers', async () => {
     // Given
     const exceptionHandlers = [
@@ -221,6 +99,7 @@ describe('Server', () => {
     assert(exceptionHandlers[1].mock.callCount(),
       'Second exception handler not called!')
   })
+
   it('should await exception handlers', async () => {
     // Given
     const delay = 50
@@ -249,6 +128,7 @@ describe('Server', () => {
     assert(exceptionHandlers[1].mock.callCount(),
       'Second exception handler not called!')
   })
+
   it('should stop exception handlers after response is finished', async () => {
     // Given
     const exceptionHandlers = [
@@ -298,32 +178,36 @@ describe('Server', () => {
       'Second exception handler not called!')
   })
 
-  it('should configure', () => {
+  it('should send default response on exception handler error', async () => {
     // Given
-    const configurer = mock.fn()
+    server.handleException(() => { throw new Error('test') })
+    server.handle('test-exception-error', () => { throw new Error('test') })
 
-    // When
-    server.configure(configurer)
+    const header = new MessageHeader({
+      subject: 'test-exception-error'
+    })
 
-    // Then
-    assert.equal(configurer.mock.callCount(), 1, 'Configurer was not called!')
-  })
-
-  it('should not listen after disconnect', async () => {
-    // Given
-    server.defaultHandlers(() => assert.fail('Handler was called!'))
-    server.disconnect(stream)
+    const expected = new Message({
+      header,
+      type: MessageTypes.Error,
+      error: new MessageError({
+        type: 'GenericError',
+        message: 'Failed processing correspondence'
+      })
+    })
 
     // When
     await send(stream, new Message({
-      header: new MessageHeader({
-        subject: 'test'
-      })
+      header
     }))
 
-    // Then fail if handled
+    // Then
+    const actual = stream.fromJSON()
+    assert.deepStrictEqual(objectify(actual), objectify(expected))
   })
+  // }}}
 
+  // {{{ Message reject
   it('should reject malformed message', async () => {
     // Given
     const handler = mock.fn()
@@ -356,7 +240,9 @@ describe('Server', () => {
     assert(calls[0]?.arguments?.[0] instanceof InvalidMessageError,
       'Wrong error emitted!')
   })
+  // }}}
 
+  // {{{ Events
   it('should emit on connect', () => {
     // Given
     const handler = mock.fn()
@@ -391,5 +277,32 @@ describe('Server', () => {
 
     // Then
     assert.equal(handler.mock.callCount(), 1, 'No event was emitted!')
+  })
+  // }}}
+
+  it('should configure', () => {
+    // Given
+    const configurer = mock.fn()
+
+    // When
+    server.configure(configurer)
+
+    // Then
+    assert.equal(configurer.mock.callCount(), 1, 'Configurer was not called!')
+  })
+
+  it('should not listen after disconnect', async () => {
+    // Given
+    server.defaultHandler(() => assert.fail('Handler was called!'))
+    server.disconnect(stream)
+
+    // When
+    await send(stream, new Message({
+      header: new MessageHeader({
+        subject: 'test'
+      })
+    }))
+
+    // Then fail if handled
   })
 })
