e3e2665d6460d2dc66d1e24acf635ff854376611
Tamas Galffy <ezittgtx@gmail.com>
2023-02-17 19:52:17 +0100

    Add Peer wrapper

---
+++ packages/nlon-websocket/lib/peer.mjs
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import * as ws from 'isomorphic-ws'
+/* eslint-enable no-unused-vars */
+
+import * as nlon from '@elementbound/nlon'
+import { WebSocketStream } from './websocket.wrapper.mjs'
+
+class WebSocketWrapperPeer extends nlon.Peer {
+  /** @type {ws.WebSocket} */
+  #socket
+
+  /**
+  * @param {ws.WebSocket} socket
+  * @param {nlon.PeerOptions} options
+  */
+  constructor (socket, options) {
+    super(new WebSocketStream(socket), options)
+
+    this.#socket = socket
+  }
+
+  /**
+  * @type {ws.WebSocket} socket
+  */
+  get socket () {
+    return this.#socket
+  }
+}
+
+/**
+* @param {ws.WebSocket} socket
+* @param {nlon.PeerOptions} options
+*/
+export function wrapWebSocketPeer (socket, options) {
+  return new WebSocketWrapperPeer(socket, options)
+}
+
+/**
+* @typedef {object} WebSocketOptions
+* @property {string} address
+* @property {string|string[]} [protocols]
+*/
+
+/**
+* @param {nlon.PeerOptions | WebSocketOptions} options
+*/
+export function createWebSocketPeer (options) {
+  const socket = new ws.WebSocket(options.address, options.protocols)
+
+  return wrapWebSocketPeer(socket, options)
+}
