d969f4fffd33fdc410b8f6ee8f1f279c8be632ff
Tamás Gálffy <ezittgtx@gmail.com>
2023-05-01 14:19:22 +0200

    fix: Peer.send to support any message type
    Originally, the method just used the stream as-is to write the message, but did not call the Correspondence's handle method, resulting in the Correspondence not being aware of what just happened.
    
    Fixes #42

--- packages/nlon/test/protocol.test.mjs
+++ packages/nlon/test/protocol.test.mjs
@@ -2,57 +2,91 @@ import { describe, it } from 'node:test'
 import assert from 'node:assert'
 import { Message, MessageHeader, MessageTypes } from '../lib/protocol.mjs'
 
-const invalidMessages = [
-  [{}, 'missing header'],
-  [{ header: {} }, 'empty header'],
-  [{ header: { correspondenceId: '' } }, 'empty correspondenceId'],
-  [{ header: { correspondenceId: 'test', subject: '' } }, 'empty subject'],
-  [{ header: { correspondenceId: 'test', subject: 'test' } }, 'missing type'],
-  [
-    { header: { correspondenceId: 'test', subject: 'test' }, type: 'foo' },
-    'invalid type'
+describe('MessageHeader', () => {
+  const invalidHeaders = [
+    [undefined, 'missing header'],
+    [{}, 'empty header'],
+    [{ correspondenceId: '' }, 'empty correspondenceId'],
+    [{ correspondenceId: 'test' }, 'missing subject'],
+    [{ correspondenceId: 'test', subject: ''}, 'empty subject'],
   ]
-]
 
-const validMessages = [
-  [
-    new Message({
-      header: new MessageHeader({
-        correspondenceId: 'test',
-        subject: 'test'
-      }),
-      type: MessageTypes.Data
-    }),
-    'without body and authorization'
-  ],
-
-  [
-    new Message({
-      header: new MessageHeader({
-        correspondenceId: 'test',
-        subject: 'test',
-        authorization: 'super-secret'
-      }),
-      type: MessageTypes.Data
-    }),
-    'without body'
-  ],
-
-  [
-    new Message({
-      header: new MessageHeader({
-        correspondenceId: 'test',
-        subject: 'test',
-        authorization: 'super-secret'
-      }),
-      type: MessageTypes.Data,
-      body: 'Hello world!'
-    }),
-    'with body'
+  const validHeaders = [
+    [{ correspondenceId: 'test', subject: 'foo' }, 'valid header'],
+    [{ correspondenceId: 'test', subject: 'foo', authorization: 'yes' }, 'with authorization'],
+    [{ correspondenceId: 'test', subject: 'foo', foo: 'bar' }, 'with custom header']
   ]
-]
+
+  for (const [message, name] of invalidHeaders) {
+    it(`should throw on validate - ${name}`, () => {
+      // When + Then
+      assert.throws(
+        () => MessageHeader.validate(message)
+      )
+    })
+  }
+
+  for (const [message, name] of validHeaders) {
+    it(`should accept on validate - ${name}`, () => {
+      // When + Then
+      assert.doesNotThrow(
+        () => MessageHeader.validate(message)
+      )
+    })
+  }
+})
 
 describe('Message', () => {
+  const invalidMessages = [
+    [{}, 'missing header'],
+    [{ header: {} }, 'empty header'],
+    [{ header: { correspondenceId: '' } }, 'empty correspondenceId'],
+    [{ header: { correspondenceId: 'test', subject: '' } }, 'empty subject'],
+    [{ header: { correspondenceId: 'test', subject: 'test' } }, 'missing type'],
+    [
+      { header: { correspondenceId: 'test', subject: 'test' }, type: 'foo' },
+      'invalid type'
+    ]
+  ]
+
+  const validMessages = [
+    [
+      new Message({
+        header: new MessageHeader({
+          correspondenceId: 'test',
+          subject: 'test'
+        }),
+        type: MessageTypes.Data
+      }),
+      'without body and authorization'
+    ],
+
+    [
+      new Message({
+        header: new MessageHeader({
+          correspondenceId: 'test',
+          subject: 'test',
+          authorization: 'super-secret'
+        }),
+        type: MessageTypes.Data
+      }),
+      'without body'
+    ],
+
+    [
+      new Message({
+        header: new MessageHeader({
+          correspondenceId: 'test',
+          subject: 'test',
+          authorization: 'super-secret'
+        }),
+        type: MessageTypes.Data,
+        body: 'Hello world!'
+      }),
+      'with body'
+    ]
+  ]
+
   for (const [message, name] of invalidMessages) {
     it(`should throw on validate - ${name}`, () => {
       // When + Then
